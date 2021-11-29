6d17708e0eb098471b3056760b67b6488f84d278
Agnes Okutoyi <agnes.okutoyi@student.moringaschool.com>
2021-11-29 14:44:08 +0300

    Add button on form

--- script.js
+++ script.js
@@ -5,15 +5,30 @@ function Pizza(name, size, topping, crust, total){
     this.crust=crust;
     this.total=total;
   }
-  $(document).ready(function () {
-    $("#check-btn").click(function () {
-        alert ("Hello, thanks for ordering with us")
-    });
-});
+
+//   $(document).ready(function () {
+//     $("#check-btn").click(function () {
+//         alert ("Hello, thanks for ordering with us")
+//     });
+// });
 //   Pizza.prototype.fullOrder= function() {
 //          return this.name + " " + this.size + " " + this.topping + " " + this.crust + " " + this.total;
 //        }
 //        console.log(fullOrder());
+
+$(document).ready(function(){
+    $("form").submit(function(event){
+        var name = $("input#name").val();
+        var location = $("input#location").val();
+        if($("input#name").val() &&$("input#location").val()){
+            alert(name +",  We have received your order and our rider will deliver at your location.Expect a call from our rider,Welcome again!!!");
+        }
+        else{
+            alert("Helloo, Kindly tell us your location");
+        }
+
+    })
+})
       
 
     
