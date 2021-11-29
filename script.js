6f4eedb00eb70c5ad3fc8e40a9de204a0bcff998
Agnes Okutoyi <agnes.okutoyi@student.moringaschool.com>
2021-11-29 07:56:20 +0300

    Modified structure

--- script.js
+++ script.js
@@ -39,56 +39,56 @@ var toppingPrice;
     default:
       console.log("Invalid"); 
   }
-  $("form").submit(function(){
-    event.preventDefault();
-    var pizza1 = function add(size1,crust1,topping1){
-    prefer1 = $(".prefer").find(":selected").val();
-    size1 = $(".siz").find(":selected").val();
-    crust1 =$(".crus").find(":selected").val();
-    // console.log(size1);
-    alert(
-        size1
-    )
-    topping1=[];
-    $.each($("input[name='top']:checked"), function(){            
-        topping1.push($(this).val());
-    });
+//   $("form").submit(function(){
+//     event.preventDefault();
+//     var pizza1 = function add(size1,crust1,topping1){
+//     prefer1 = $(".prefer").find(":selected").val();
+//     size1 = $(".siz").find(":selected").val();
+//     crust1 =$(".crus").find(":selected").val();
+//     // console.log(size1);
+//     alert(
+//         size1
+//     )
+//     topping1=[];
+//     $.each($("input[name='top']:checked"), function(){            
+//         topping1.push($(this).val());
+//     });
     
-  toppingPrice=topping1.length * 100;
-  total=pricing + crustPrice + toppingPrice;
-  return parseInt(pricing) + parseInt(crustPrice) + toppingPrice;
+//   toppingPrice=topping1.length * 100;
+//   total=pricing + crustPrice + toppingPrice;
+//   return parseInt(pricing) + parseInt(crustPrice) + toppingPrice;
 
-}
-  function pizzaAmount(){
-    var pizzaNumber= $(".quantify").val();
-    return pizza1() * parseInt(pizzaNumber);
-  }
+// }
+//   function pizzaAmount(){
+//     var pizzaNumber= $(".quantify").val();
+//     return pizza1() * parseInt(pizzaNumber);
+//   }
 
-  $("#enter").click(function(){
-    return pizzaAmount();
-  });
-  $(".showPrice").text("The current price is " + pizzaAmount());
-
-
-  $("#deliver").slideDown(3000);
-  $("#check").click(function(){
-    var destination = $("input.locate").val();
-    function totalForDelivery(){
-     return pizzaAmount() + 200;
- }
-  $("#totalDelivery").text("Total price is " + totalForDelivery());
-  $("#message").text("Thank you."+ "Your order will be delivered at " + destination)
- });
- var order= new Pizza(prefer1, size1, topping1, crust1, total);
-
- console.log(size1);
-$("#myOrder").append('<tr><td class="">'+ order.name +'</td><td class="pSize">' + order.size + '</td><td class="pCrust">'+order.crust + '</td><td class="pTopping">'+order.topping+'</td><td class="totals">'+order.total+'</td></tr>');
+//   $("#enter").click(function(){
+//     return pizzaAmount();
+//   });
+//   $(".showPrice").text("The current price is " + pizzaAmount());
 
 
-  Pizza.prototype.fullOrder= function() {
-    return this.name + " " + this.size + " " + this.topping + " " + this.crust + " " + this.total;
-  }
-  console.log(fullOrder());
+//   $("#deliver").slideDown(3000);
+//   $("#check").click(function(){
+//     var destination = $("input.locate").val();
+//     function totalForDelivery(){
+//      return pizzaAmount() + 200;
+//  }
+//   $("#totalDelivery").text("Total price is " + totalForDelivery());
+//   $("#message").text("Thank you."+ "Your order will be delivered at " + destination)
+//  });
+//  var order= new Pizza(prefer1, size1, topping1, crust1, total);
+
+//  console.log(size1);
+// $("#myOrder").append('<tr><td class="">'+ order.name +'</td><td class="pSize">' + order.size + '</td><td class="pCrust">'+order.crust + '</td><td class="pTopping">'+order.topping+'</td><td class="totals">'+order.total+'</td></tr>');
+
+
+//   Pizza.prototype.fullOrder= function() {
+//     return this.name + " " + this.size + " " + this.topping + " " + this.crust + " " + this.total;
+//   }
+//   console.log(fullOrder());
   
   
-});
+// });
